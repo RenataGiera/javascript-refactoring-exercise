9157b409d1814777345198f010fde71a6ddd6704
Renata <renatagiera@gmail.com>
2023-10-25 14:36:19 +0100

    style: implemented changes and comments

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,52 +1,63 @@
-var txr = [];
-
 function processTransactions(transActions) {
+  let txr = []; // to make it block / not global scope
+  // txr = []; has been already declared
 
-    txr = [];
+  if (!validateTransactions(transActions)) {
+    throw new Error("Undefined collection of transactions");
+  }
 
-    if(!validateTransactions(transActions)) {
-        throw new Error("Undefined collection of transactions")
-    }
+  let txCount = {}; //to make it block scope, must me let becouse we reassign it later
 
-    let txCount = {}
+  //const numberOfTransactions = transActions.length; /*  we don't need this for the forEach syntax */
 
-    const numberOfTransactions = transActions.length;
+  /*for (let i = 0; i < numberOfTransactions; i++) { 
+    const transaction = transActions[i];
+    txCount[transaction]
+      ? (txCount[transaction] += 1)
+      : (txCount[transaction] = 1);
+  }*/
+  transActions.forEach((transaction) => {
+    //implemented forEach to make if shorter
+    txCount[transaction]
+      ? (txCount[transaction] += 1)
+      : (txCount[transaction] = 1);
+  });
 
-    for(var i = 0; i < numberOfTransactions; i++) {
-        const transaction = transActions[i];
-        txCount[transaction] ? txCount[transaction] += 1 : txCount[transaction] = 1;
-    }
+  txCount = sortByAmountThenName(txCount); // pass the object by reference
 
-    txCount = sortByAmountThenName(txCount);
-    
-    // Place them back in array for returning
-    Object.keys(txCount).forEach(function (key, index) {
-        txr[index] = `${key} ${txCount[key]}`;
-    });
-
-    return txr;
+  // Place them back in array for returning
+  Object.keys(txCount).forEach( (key, index) => {
+    txr[index] = `${key} ${txCount[key]}`;
+  });
+  return txr;
 }
 
-function sortByAmountThenName(txCount) {
-    let sortedKeys = Object.keys(txCount).sort(function sortingFunction(itemOne, itemTwo) {
-        return  txCount[itemTwo] - txCount[itemOne] || itemOne > itemTwo || -(itemOne < itemTwo)}
+sortByAmountThenName(txCount) => {(
+  let sortedKeys = Object.keys(txCount).sort(function sortingFunction(
+    itemOne,
+    itemTwo
+  ) {
+    return (
+      txCount[itemTwo] - txCount[itemOne] ||
+      itemOne > itemTwo ||
+      -(itemOne < itemTwo)
     );
+  });
 
-    let sortedResults = {};
-    for(let objectKey of sortedKeys) {
-        sortedResults[objectKey] = txCount[objectKey];
-    }
+  const sortedResults = {};
+//   for (let objectKey of sortedKeys) {
+//     sortedResults[objectKey] = txCount[objectKey];
+//   }
+     sortedKeys.forEach((objectKey)=>{
+    sortedResults[objectKey] = txCount[objectKey];
+});
 
-    return sortedResults;
+
+ return sortedResults;
 }
-
 
 function validateTransactions(transactions) {
-    if(transactions === undefined) {
-        return false;
-    } 
-
-    return true;
+   return (transactions !== undefined) //simplified the check
 }
 
 module.exports = processTransactions;
